6d616812510442eab0687927614678bee6196e66
Alexey Fishler <amberdance@yandex.ru>
2023-05-28 15:41:36 +0300

    vuejs client

---
+++ vuejs-client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <el-container class="container">
+      <UsersList />
+    </el-container>
+  </div>
+</template>
+
+<script>
+import UsersList from "@/components/UsersList";
+
+export default {
+  name: "app",
+  components: {
+    UsersList,
+  },
+};
+</script>
